8526715cadb54668c1e8ba80d9e4fbe907cf32da
Haraldurs20 <haraldurs20@ru.is>
2020-12-01 12:11:10 +0000

    split classes

---
+++ Data classes/Customer_class.py
@@ -0,0 +1,8 @@
+class Customer(object):
+    def __init__(self,ID,Customer_name,License_type,GBP,BBP) -> None:
+        self.id = ID
+        self.costumer_name = Customer_name
+        self.License_type = License_type
+        self.GBP = GBP
+        self.BBP = BBP
+
---
+++ Data classes/Destination_class.py
@@ -0,0 +1,7 @@
+class Destination(object):
+    def __init__(self,ID,Destination_name,Phone,opening_hours,reportfilename) -> None:
+        self.id = ID
+        self.Destination = Destination_name
+        self.Phone = Phone
+        self.opening_hours = opening_hours
+        self.report_filename = reportfilename
