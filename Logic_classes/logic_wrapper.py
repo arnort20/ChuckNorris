4592cb672d6ae07a3bfa2caf34b018736f029d42
arnort20 <70693271+arnort20@users.noreply.github.com>
2020-12-02 10:51:54 +0000

    import lagað

--- Logic_classes/logic_wrapper.py
+++ Logic_classes/logic_wrapper.py
@@ -1,10 +1,6 @@
-#from vehicle_logic import *
-#from contract_logic import Conrtact as contLogic
-#from Data_classes.Logic_classes.contract_logic import Contract as contLogic
+from .vehicle_logic import 
 from .contract_logic import Contract as contLogic
-#import customer_logic
-#import destination_logic
-#import employee_logic
+
 
 class LogicAPI:
     def __init__(self):
