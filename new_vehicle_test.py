e9b603af2044c59631d3a9d6a2c7f4c5549ba924
arnort20 <70693271+arnort20@users.noreply.github.com>
2020-12-02 15:09:14 +0000

    .

--- new_vehicle_test.py
+++ new_vehicle_test.py
@@ -1,4 +1,5 @@
 from Logic_classes.logic_wrapper import LogicAPI as lAPI
 
 api = lAPI()
-api.make_new_vehicle("gr 425","small","toyota Jimny","Green","2011",1.24)
+api.make_new_vehicle("gr 425","minijeep","toyota","Jimny","green","2020","1.24","3")
+
