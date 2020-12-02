e2f77b589567159855181afd21c5ac5c8e57c95a
arnort20 <70693271+arnort20@users.noreply.github.com>
2020-12-02 10:56:32 +0000

    .

--- Logic_classes/logic_wrapper.py
+++ Logic_classes/logic_wrapper.py
@@ -1,9 +1,10 @@
-from .vehicle_logic import 
-from .contract_logic import Contract as contLogic
-
-
+from .vehicle_logic import Vehicle as vehi_logic
+from .contract_logic import Contract as cont_logic
+from .employee_logic import Employee as emp_logic
+from .destination_logic import Destination as dest_logic
+from .customer_logic import Customer as cust_logic
 class LogicAPI:
     def __init__(self):
-        self.contract = contLogic()
+        self.contract = cont_logic()
     def get_contract(self, contractID):
         return self.contract.getContract(contractID)
