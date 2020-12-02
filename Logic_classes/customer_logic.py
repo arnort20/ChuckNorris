636cdc30869351bc4908f2a325acd46ee493f85c
ingvar20 <74902597+ingvar20@users.noreply.github.com>
2020-12-02 14:35:19 +0000

    Update customer_logic.py

--- Logic_classes/customer_logic.py
+++ Logic_classes/customer_logic.py
@@ -4,17 +4,13 @@ class Customer:
     def __init__(self):
         self.api = dAPI()
     def get_customer(self,customerID):
-        pass
-    def get_customer_txt(self,customerID):
-        pass
+        return self.dAPI.bring_customer(customerID)
     def new_customer(self,customerID,name,license_type):
         dAPI.make_customer(customerID,name,license_type,0,0)
     def add_GBP(self,customerID,added_points):
-        #make changes
-        pass
+        return self.dAPI.add_points(customerID,added_points)
     def add_BBP(self,customerID,added_points):
-        #make changes
-        pass
+        return self.dAPI.add_points(customerID,added_points)
     def change_name(self,customerID,new_name):
         #make changes
         pass
