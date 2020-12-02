0f1bc9ef6949054bcc28df723fc35f11d40703ec
arnort20 <70693271+arnort20@users.noreply.github.com>
2020-12-02 10:26:21 +0000

    .

--- Logic_classes/logic_wrapper.py
+++ Logic_classes/logic_wrapper.py
@@ -1,5 +1,6 @@
 #from vehicle_logic import *
-from contract_logic import Conrtact as contLogic
+#from contract_logic import Conrtact as contLogic
+from Data_classes.Logic_classes.contract_logic import Contract as contLogic
 #import customer_logic
 #import destination_logic
 #import employee_logic
