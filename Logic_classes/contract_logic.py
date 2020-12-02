d2db9d252353518a7bfab11758bd7a3d5c90edec
arnort20 <70693271+arnort20@users.noreply.github.com>
2020-12-02 09:57:53 +0000

    .

--- Logic_classes/contract_logic.py
+++ Logic_classes/contract_logic.py
@@ -1,7 +1,8 @@
-from Data_classes.DataAPI import DataAPI
+#from Data_classes.DataAPI import DataAPI
+from Data_classes.DataAPI import DataAPI as dAPI
 
 class Contract:
     def __init__(self):
-        self.dAPI = DataAPI.DataAPI()
+        self.dAPI = dAPI.DataAPI()
     def getContract(self, contractID):
         return self.dAPI.get_contract(contractID)
--- test.py
+++ test.py
@@ -1,5 +1,5 @@
-from Data_classes.DataAPI import DataAPI
-api = DataAPI()
+from Logic_classes.logic_wrapper import LogicAPI as lAPI
+api = lAPI()
 contract = api.get_contract("123")
 print(contract)
 print(api.get_contracts())
