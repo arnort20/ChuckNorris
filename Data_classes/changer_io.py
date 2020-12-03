5abdcf20cb5ca8dd71a1b7175b2e1b667ecd9e24
Haraldurs20 <haraldurs20@ru.is>
2020-12-03 13:44:09 +0000

    did changes

--- Data_classes/changer_io.py
+++ Data_classes/changer_io.py
@@ -5,3 +5,15 @@ from Data_classes.deleter_io import Dell
 class Changer(object):
     def __init__(self) -> None:
         pass
+    
+    def change(filename,id,changes):
+        item = Getter.get_certein(id,filename)
+        new_item = item
+
+        for key,value in changes.items():
+            new_item[key] = value
+
+        contracts = Getter.get_csv(filename)
+        contracts[int(id)-1] = new_item
+        Dell.rewrite_file(contracts,contracts,filename)
+        
--- test.py
+++ test.py
@@ -1,14 +1,21 @@
 from Data_classes.DataAPI import DataAPI
 api = DataAPI()
-# apid = api.vehicles_makeID() 
-# api.add_vehicle(apid,"jojo","jingle","jambajuice","akakaka","blue","25","20","yes","12","12")
+
+api.change_vehicle("1",{"Vehicle name": "arnar"})
+
+apid = api.vehicles_makeID() 
+api.add_vehicle(apid,"jojo","jingle","jambajuice","akakaka","blue","25","20","yes","12","12")
+
+for item in api.get_contracts():
+    print(item)
 
 
-#print(api.get_vehicles())
+
+print(api.get_vehicles())
 print(api.get_contract("1"))
 
-#api.delete_vehicle("1")
-# for item in range(1,len(api.get_vehicles())+1):
-#      api.delete_vehicle(str(item))
-#det = api.get_customer("12")
-# print(det)
+api.delete_vehicle("1")
+for item in range(1,len(api.get_vehicles())+1):
+     api.delete_vehicle(str(item))
+det = api.get_customer("12")
+print(det)
