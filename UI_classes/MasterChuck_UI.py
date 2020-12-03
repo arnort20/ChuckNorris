3ab47ab4b932c9c29e257c5d9c47e2b860f11631
arnardecay <arnarj19@ru.is>
2020-12-03 11:17:21 +0000

    addad MasterChuck

---
+++ UI_classes/MasterChuck_UI.py
@@ -0,0 +1,11 @@
+from UI_classes.Login_UI import LoginUI
+
+
+class Master_login():
+    def __init__(self):
+        pass
+
+    def chuck_login(self):
+        print("""
+        Welcome Master Chuck! """)
+        
