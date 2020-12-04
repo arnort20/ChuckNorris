4a980dff4ed2aaf52191fd70cb06d7f0607bede1
arnort20 <70693271+arnort20@users.noreply.github.com>
2020-12-04 11:37:29 +0000

    Update logic_wrapper.py

--- Logic_classes/logic_wrapper.py
+++ Logic_classes/logic_wrapper.py
@@ -3,6 +3,7 @@ from .contract_logic import Contract as cont_logic
 from .employee_logic import Employee as emp_logic
 from .destination_logic import Destination as dest_logic
 from .customer_logic import Customer as cust_logic
+import calendar
 
 class LogicAPI:
     def __init__(self, userID, pword):
