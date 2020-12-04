fbe3b6c17fd2183958d7e483c85b1b2eecc61eef
arnort20 <70693271+arnort20@users.noreply.github.com>
2020-12-04 14:05:49 +0000

    Update logic_wrapper.py

--- Logic_classes/logic_wrapper.py
+++ Logic_classes/logic_wrapper.py
@@ -58,42 +58,15 @@ class LogicAPI:
 
     #make the contract:
     #vehicles_ID must be a list of IDs (if multiple) comma seperated with no whitespace
-    def make_new_contract(self, customer_ID, vehicles_ID, start_date, end_date):
-        vehicles = vehicles_ID.split(',')
-        #get all the vehicles and put em in a list of objects
-        signed_vehicles = []
-        for vehicleID in vehicles:
-            Vehicle = self.get_vehicle(vehicleID)
-            signed_vehicles.append(Vehicle)
+    def make_new_contract(self, customer_ID, vehicle_ID, start_date, end_date):
+
+        Vehicle = self.get_vehicle(vehicle_ID)
         #check if the customer has the appropriate licensing
-        for vehicle in signed_vehicles:
-            if not self.check_license(customer_ID, vehicle.id):
-                return "license_error"
-        #customer has the appropriate license for all vehicles, reserve them all
-        #for vehicle in signed_vehicles:
-            #self.reserve_vehicle(vehicle.id, start_date, end_date)
-            #remember to create reservation function
-        self.new_contract(self.user, customer_ID, vehicles_ID, start_date, end_date)
+        if not self.check_license(customer_ID, Vehicle.id):
+            return "license_error"
+        #customer has the appropriate license for the vehicle, reserve it
+        self.new_contract(self.user, customer_ID, vehicle_ID, start_date, end_date)
         return "success"
-
-    #when the customer picks up the vehicles:
-    #for all the cars:
-    #   available = False
-
-    #when returning the vehicle/s
-    def return_vehicles(self, contractID, date_returned):
-        Contract = self.get_contract(contractID)
-        end_date = Contract.end_date
-        # (function that checks if the date returned is later 
-        # than the contract's end date goes here)
-        #apply BBP to customer if True
-        vehicles = Contract.vehicle_ID
-        for vehicle in vehicles:
-            self.return_vehicle(vehicle.id)
-
-    def return_vehicle(self,vehicleID):
-        pass
-        #returns True if it succeeds, otherwise false
         
     def use_GBP(self, customer_ID):
         Customer = self.get_customer(customer_ID)
