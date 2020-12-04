649e4a117c6afec5259485423f75988ac0e452b5
ingvar20 <74902597+ingvar20@users.noreply.github.com>
2020-12-04 13:51:13 +0000

    Update employee_logic.py

--- Logic_classes/employee_logic.py
+++ Logic_classes/employee_logic.py
@@ -8,8 +8,9 @@ class Employee:
         employeeID = dAPI.get_employeeID()
         dAPI.add_employee(employeeID,Employee_name,ssn,Address,Phone,Email,Location,password)
     def fire(self,emp_ID):
+        self.dAPI.delete_employee(emp_ID)
         #removes employee form database
-        pass
+        
     def change_employee(self, emp_ID, change_dict):
         dAPI.change_Employee(emp_ID, change_dict)
         
