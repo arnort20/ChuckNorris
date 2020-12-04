687faf6d194e03d914d7ede0c98c942f98b34da1
ingvar20 <74902597+ingvar20@users.noreply.github.com>
2020-12-04 14:38:44 +0000

    Update logic_wrapper.py

--- Logic_classes/logic_wrapper.py
+++ Logic_classes/logic_wrapper.py
@@ -54,7 +54,10 @@ class LogicAPI:
         self.destination = dest_logic()  
 
 
-    
+    #bill things
+    def make_bill(self,contract_ID):
+        contract_ID.start_date
+        contract_ID.end_date
 
     #make the contract:
     #vehicles_ID must be a list of IDs (if multiple) comma seperated with no whitespace
