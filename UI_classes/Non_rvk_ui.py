4353f239408bd088938a417e3a082e4847ca7624
arnort20 <70693271+arnort20@users.noreply.github.com>
2020-12-11 12:00:33 +0000

    bugfix recieve vehicle

--- UI_classes/Non_rvk_ui.py
+++ UI_classes/Non_rvk_ui.py
@@ -189,7 +189,7 @@ class Non_rvk_ui:
             else:
                 gbp_used = False
 
-            self.logic.recieve_vehicle(returning_contract_ID,return_date, gbp_used)
+            
             late_return = self.logic.recieve_vehicle(returning_contract_ID,return_date,gbp_used)
 
 
